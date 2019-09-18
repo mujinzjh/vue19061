0af7adabbbf2fac4fae330f6023afa0eab8db19a
1793543683 <1793543683@qq.com>
2019-09-18 21:15:25 +0800

    写登录注册界面时对main.js的更改

--- myvue/src/main.js
+++ myvue/src/main.js
@@ -2,12 +2,28 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import topHead from "./components/topHead"
+import bottomfooter from "./components/bottomfooter"
+
+
+
 Vue.use(ElementUI);
 Vue.config.productionTip = false
+
+//定义全局axios的公用服务端口
+axios.defaults.baseURL="http://localhost:8888"
+
+//向原型上追加通用方法
+Vue.prototype.axios=axios
+
+//定义全局组件
+Vue.component("bottom-footer",bottomfooter)
 Vue.component("top-head",topHead);
+
+
 new Vue({
   router,
   store,
